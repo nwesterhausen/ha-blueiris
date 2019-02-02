68d9ece832b3a92b29e5b39aa00507615d7aaea7
Nicholas Westerhausen <nicholas@westerhausen.me>
2019-02-02 08:16:01 -0500

    proper (?) async methods in camera

--- blue_iris/__init__.py
+++ blue_iris/__init__.py
@@ -4,16 +4,15 @@ Component for connecting Home Assistant to a Blue Iris server.
 import voluptuous as vol
 
 from homeassistant.helpers import aiohttp_client, config_validation as cv
-from homeassistant.const import (
-    CONF_NAME, CONF_HOST, CONF_PORT, CONF_USERNAME, CONF_PASSWORD
-)
+from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_PORT,
+                                 CONF_USERNAME, CONF_PASSWORD)
 import logging
 
 DOMAIN = 'blue_iris'
 _LOGGER = logging.getLogger(__name__)
 
 CONF_PROTOCOL = 'protocol'
-REQUIREMENTS = ['pyblueiris==0.2.10']
+REQUIREMENTS = ['pyblueiris==0.2.11']
 
 DEFAULT_PROTOCOL = "http"
 DEFAULT_PORT = 1  # This is set as the default, a 1 is changed to 80 for http and 443 for https
@@ -34,14 +33,18 @@ ATTR_API_URL = "JSON API Endpoint"
 
 # The config that zone accepts is the same as if it has platforms.
 CONFIG_SCHEMA = vol.Schema({
-    DOMAIN: vol.Schema({vol.Required(CONF_USERNAME): cv.string,
-                        vol.Required(CONF_PASSWORD): cv.string,
-                        vol.Required(CONF_HOST): cv.string,
-                        vol.Optional(CONF_PROTOCOL, default=DEFAULT_PROTOCOL): cv.string,
-                        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-                        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
-                        })
-}, extra=vol.ALLOW_EXTRA)
+    DOMAIN:
+    vol.Schema(
+        {
+            vol.Required(CONF_USERNAME): cv.string,
+            vol.Required(CONF_PASSWORD): cv.string,
+            vol.Required(CONF_HOST): cv.string,
+            vol.Optional(CONF_PROTOCOL, default=DEFAULT_PROTOCOL): cv.string,
+            vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+            vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string
+        })
+},
+                           extra=vol.ALLOW_EXTRA)
 
 
 async def async_setup(hass, config):
@@ -63,30 +66,33 @@ async def async_setup(hass, config):
 
     websession = aiohttp_client.async_get_clientsession(hass)
 
-    hass.data[DOMAIN] = blue = bi(websession,
-                                  conf.get(CONF_USERNAME),
-                                  conf.get(CONF_PASSWORD),
-                                  conf.get(CONF_PROTOCOL),
-                                  conf.get(CONF_HOST),
-                                  port,
-                                  debug=True,
-                                  logger=_LOGGER)
+    hass.data[DOMAIN] = blue = bi(
+        websession,
+        conf.get(CONF_USERNAME),
+        conf.get(CONF_PASSWORD),
+        conf.get(CONF_PROTOCOL),
+        conf.get(CONF_HOST),
+        port,
+        debug=True,
+        logger=_LOGGER)
 
     login_successful = await hass.data[DOMAIN].setup_session()
     if not login_successful:
         _LOGGER.error("Failed to login to server")
         return False
-    _LOGGER.info("Connected to {} running Blue Iris v.{}".format(hass.data[DOMAIN].name,
-                                                                 hass.data[DOMAIN].version))
+    _LOGGER.info("Connected to {} running Blue Iris v.{}".format(
+        hass.data[DOMAIN].name, hass.data[DOMAIN].version))
     await blue.update_all_information()
-    hass.states.async_set(entity_id=ENTITY_ID_FORMAT.format(blue.name),
-                                new_state=STATE_CONNECTED,
-                                attributes={ATTR_ADMIN: blue.admin,
-                                            ATTR_NAME: blue.name,
-                                            ATTR_PROFILES: blue.attributes["profiles"],
-                                            ATTR_SCHEDULES: blue.attributes["schedules"],
-                                            ATTR_VERSION: blue.version,
-                                            ATTR_NUM_CAMERAS: len(blue.cameras),
-                                            ATTR_API_URL: blue.url}
-                                )
+    hass.states.async_set(
+        entity_id=ENTITY_ID_FORMAT.format(blue.name),
+        new_state=STATE_CONNECTED,
+        attributes={
+            ATTR_ADMIN: blue.admin,
+            ATTR_NAME: blue.name,
+            ATTR_PROFILES: blue.attributes["profiles"],
+            ATTR_SCHEDULES: blue.attributes["schedules"],
+            ATTR_VERSION: blue.version,
+            ATTR_NUM_CAMERAS: len(blue.cameras),
+            ATTR_API_URL: blue.url
+        })
     return True
--- blue_iris/camera.py
+++ blue_iris/camera.py
@@ -3,9 +3,10 @@ Sets up the Blue Iris camera streams
 """
 import logging
 
+from homeassistant.components.camera import SUPPORT_ON_OFF
+from homeassistant.components.camera.mjpeg import CONF_MJPEG_URL, MjpegCamera, filter_urllib3_logging
 from homeassistant.const import CONF_NAME, CONF_VERIFY_SSL
-from homeassistant.components.camera.mjpeg import (
-    CONF_MJPEG_URL, MjpegCamera)
+
 # from homeassistant.components.blue_iris import DOMAIN as BLUEIRIS_DOMAIN
 
 BLUEIRIS_DOMAIN = 'blue_iris'
@@ -18,6 +19,7 @@ DEPENDENCIES = ['blue_iris']
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Blue Iris cameras"""
     import pyblueiris as bi
+    filter_urllib3_logging()
 
     bi_client: bi.BlueIris = hass.data[BLUEIRIS_DOMAIN]
     await bi_client.update_camlist()
@@ -29,7 +31,8 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     camera_streams = []
     for i in range(len(cameras)):
         cam: bi.BlueIrisCamera = cameras[i]
-        _LOGGER.info("Initializing camera {}: {}".format(cam.short_name, cam.display_name))
+        _LOGGER.info("Initializing camera {}: {}".format(
+            cam.short_name, cam.display_name))
         await cam.update_camconfig()
         camera_streams.append(BlueIrisCamera(cam))
 
@@ -41,17 +44,17 @@ class BlueIrisCamera(MjpegCamera):
     """Representation of a BlueIris camera stream"""
     from pyblueiris import BlueIrisCamera as bicam
 
-    async def turn_off(self):
-        await self.camera.disable()
+    def async_turn_off(self):
+        self.camera.disable()
 
-    async def turn_on(self):
-        await self.camera.enable()
+    def async_turn_on(self):
+        self.camera.enable()
 
-    async def enable_motion_detection(self):
-        await self.camera.detect_motion(True)
+    def async_enable_motion_detection(self):
+        self.camera.detect_motion(True)
 
-    async def disable_motion_detection(self):
-        await self.camera.detect_motion(False)
+    def async_disable_motion_detection(self):
+        self.camera.detect_motion(False)
 
     def __init__(self, camera: bicam):
         """Initialize subclass of MjpecCamera"""
@@ -59,8 +62,23 @@ class BlueIrisCamera(MjpegCamera):
             CONF_NAME: camera.display_name,
             CONF_MJPEG_URL: camera.mjpeg_url
         }
-        _LOGGER.info("Trying to setup MJPEG for {}, {}".format(camera.display_name, device_info))
+        _LOGGER.info("Trying to setup MJPEG for {}, {}".format(
+            camera.display_name, device_info))
         super().__init__(device_info)
         self._is_recording = camera.is_recording
         self._is_available = camera.is_enabled
         self.camera = camera
+
+    @property
+    def is_recording(self):
+        """Return if the camera is recording."""
+        return self.camera.is_recording
+    
+    @property
+    def is_on(self):
+        return self.camera.is_enabled
+
+    @property
+    def supported_features(self):
+        return SUPPORT_ON_OFF
+
